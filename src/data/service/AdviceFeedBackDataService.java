dca372e64c986f01f51795e2092609edd0d47c7b
ZhiTingXin <151250170@smail.nju.edu.cn>
2016-11-26 20:34:03 +0800

    again

--- src/data/service/AdviceFeedBackDataService.java
+++ src/data/service/AdviceFeedBackDataService.java
@@ -4,12 +4,11 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import PO.AdviceFeedBackPO;
-import other.ResultMessage;
 
 public interface AdviceFeedBackDataService extends Remote{
 
-	public ResultMessage addAdvice(AdviceFeedBackPO advicefeedback)throws RemoteException;
-	public ResultMessage modiffiAdvice(AdviceFeedBackPO advicefeedback)throws RemoteException;
+	public boolean addAdvice(AdviceFeedBackPO advicefeedback)throws RemoteException;
+	public boolean modiffiAdvice(AdviceFeedBackPO advicefeedback)throws RemoteException;
 	public ArrayList<AdviceFeedBackPO> getAll(String userId)throws RemoteException;
 	public ArrayList<AdviceFeedBackPO> getUnProcessed(String staffId)throws RemoteException;
 	public ArrayList<AdviceFeedBackPO> getProcessed(String staffId)throws RemoteException;
--- src/data/service/impl/OrderDataServiceImpl.java
+++ src/data/service/impl/OrderDataServiceImpl.java
@@ -1,7 +1,35 @@
 package data.service.impl;
 
+import java.util.List;
+
+import PO.OrderPO;
 import data.service.OrderDataService;
 
 public class OrderDataServiceImpl implements OrderDataService{
+
+	public boolean add(OrderPO order) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean update(OrderPO order) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean dalete(OrderPO order) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public OrderPO findorder(String orderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<OrderPO> findOrders(String userId, String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
